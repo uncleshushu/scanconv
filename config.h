5b97d6505829418a7432ff7b1a67892dff59656e
shushu <shushu.jiang@outlook.com>
2018-06-05 15:00:14 +0800

    ignores config.h

--- config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define DEBUG  /* use cmake -DRELEASE=ON to undef */
-#define TIMING /* use cmake -DTIMING=OFF to undef*/
-#define USE_OPENCL
-
-#endif  /* CONFIG_H */
